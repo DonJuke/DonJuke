7904e435fb5830e7eb3281c8a65ec17191b54ece
DonJuke <102258546+DonJuke@users.noreply.github.com>
2024-02-10 00:33:29 -0600

    Add files via upload

---
+++ Python/get_audio.py
@@ -0,0 +1,33 @@
+import speech_recognition as sr
+import gtts
+from playsound import playsound
+import os
+from datetime import datetime
+
+r = sr.Recognizer()
+
+def get_audio():
+    with sr.Microphone() as source:
+        print("Say something")
+        audio = r.listen(source)
+    return audio
+
+def audio_to_text(audio):
+    text = ""
+    try:
+        text = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Speech recognition could not understand audio")
+    except sr.RequestError:
+        print("could not request results from API")
+    return text
+
+def play_sound(text):
+    try:
+        tts = gtts.gTTS(text)
+        tempfile = "./temp.mp3"
+        tts.save(tempfile)
+        playsound(tempfile)
+        os.remove(tempfile)
+    except AssertionError:
+        print("could not play sound")
